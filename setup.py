cee67327200ca9d662a3fa0b8db800c47cd3194b
Sergio Garcia <sergiogcx@gmail.com>
2020-01-28 12:48:16 -0600

    Added package setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atd-mds-client",
+    version="0.0.1",
+    author="City of Austin",
+    author_email="transportation.data@austintexas.gov",
+    description="A Python utility to interact data endpoints compliant with the Mobility Data Specification, as designed by the Open Mobility Foundation",
+    install_requires=[
+      'requests',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cityofaustin/atd-mds-client/tree/atd-mds-client",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ),
+)
